afc10b054c791f4a0af7037297dbe1d04a2dba14
eliaszpiotr <Eliasz.Piotr02@gmail.com>
2024-04-03 10:08:45 +0200

    sprint class

--- src/main/java/com/uep/wap/model/Sprint.java
+++ src/main/java/com/uep/wap/model/Sprint.java
@@ -1,4 +1,81 @@
 package com.uep.wap.model;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "sprints")
 public class Sprint {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long sprintId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Temporal(TemporalType.DATE)
+    private Date startDate;
+
+    @Temporal(TemporalType.DATE)
+    private Date endDate;
+
+    // Konstruktor bezargumentowy jest wymagany przez JPA
+    public Sprint() {
+    }
+
+    // Konstruktor z argumentami dla łatwiejszego tworzenia obiektów
+    public Sprint(Project project, String name, Date startDate, Date endDate) {
+        this.project = project;
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    // Gettery i settery
+    public Long getSprintId() {
+        return sprintId;
+    }
+
+    public void setSprintId(Long sprintId) {
+        this.sprintId = sprintId;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+
 }
